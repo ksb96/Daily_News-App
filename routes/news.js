33afe59b7f93a4b84c85a4f4027dc7928fe28514
kubstar <52975077+kubstar@users.noreply.github.com>
2022-08-06 14:29:36 +0530

    commit init

---
+++ routes/news.js
@@ -0,0 +1,69 @@
+const express = require('express')
+const axios = require('axios')
+const news = express.Router()
+const moment = require('moment')
+const math = require('math')
+
+
+news.get('/', async (req, res) => {
+    try {
+        var url = 'http://newsapi.org/v2/top-headlines?' + 'country=in&' + 'apiKey=e973b1ec238e4da39586b016c46d221b';
+        const news_get = await axios.get(url)
+        res.render('news', {
+            articles: news_get.data.articles
+        })
+
+    } catch (error) {
+        if (error.response) {
+            console.log(error)
+        }
+
+    }
+})
+
+news.post('/search', async (req, res) => {
+    const search = req.body.search
+    //console.log(req.body.search)
+
+
+    try {
+        var url = `http://newsapi.org/v2/everything?q=${search}&apiKey=e973b1ec238e4da39586b016c46d221b`
+
+        const news_get = await axios.get(url)
+        res.render('news', {
+            articles: news_get.data.articles
+        })
+
+
+
+
+
+    } catch (error) {
+        if (error.response) {
+            console.log(error)
+        }
+
+    }
+})
+
+news.get('/news/:category', async (req, res) => {
+    var category = req.params.category;
+    try {
+        var url = 'http://newsapi.org/v2/top-headlines?country=in&category=' + category + '&apiKey=e973b1ec238e4da39586b016c46d221b';
+
+        const news_get = await axios.get(url)
+        res.render('category', {
+            articles: news_get.data.articles
+        })
+
+    } catch (error) {
+        if (error.response) {
+            console.log(error)
+        }
+
+    }
+})
+
+news.get
+
+module.exports = news
