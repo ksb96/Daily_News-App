b50391fa98d10268fa02f26b9ccb96609a0a3a8a
kubstar <52975077+kubstar@users.noreply.github.com>
2023-03-13 22:42:28 +0530

    formatting

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const path = require('path')
 const express = require('express')
-const port = process.env.PORT||3000;
+const port = process.env.PORT || 3000;
 const moment = require('moment')
 const favicon = require('serve-favicon');
 
 //init app
-const app=express()
+const app = express()
 
 //init local-moment
 app.locals.moment = moment;
@@ -17,15 +17,15 @@ app.use(favicon(path.join(__dirname, 'public', 'world-news.ico')))
 app.use(express.static(path.join(__dirname, 'public')))
 
 //set view-engine(EJS)
-app.set('view engine','.ejs')
-app.set('views','./views')
+app.set('view engine', '.ejs')
+app.set('views', './views')
 
 app.use(express.urlencoded({ extended: true }));
 
 //load routes
-app.use('/',require('./routes/news'))
+app.use('/', require('./routes/news'))
 
-app.listen(port,()=> {
+app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`)
 })
 
